a5099f498347e1bcf309451ac11298ec1cd1b2b9
maoai_xianyu <194264514@qq.com>
2019-03-11 23:48:21 +0800

    显示

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/demo/listview_demo.dart';
-import 'package:flutter_learn/demo/drawer_demo.dart';
-import 'package:flutter_learn/demo/bottom_navigation_bar_demo.dart';
+import 'demo/drawer_demo.dart';
+import 'demo/bottom_navigation_bar_demo.dart';
+import 'demo/listview_demo.dart';
+import 'demo/basic_demo.dart';
+import 'demo/layout_demo.dart';
+import 'demo/view_demo.dart';
+import 'demo/sliver_demo.dart';
+/* void main(){
+  runApp(App());
+} */
 
 void main() => runApp(App());
 
+// 自定义的小部件是一个类
+// 1. 继承 sfw StatefulWidget 需要修改数据的的小部件
+// 2. 继承 ssw StatelessWidget 不需要修改数据的小部件
+
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      home: Home(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.yellow,
-        splashColor: Colors.white70, //水波纹的效果
-      ),
-      home: HomePage(),
+          primarySwatch: Colors.yellow, // 主题的主要颜色
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5)), // 高亮颜色
+      //splashColor: Colors.white70,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+/**
+ * 使用标签展示内容,需要三样东西
+ * 1. Tab
+ * 2. TabBar 标签栏
+ * 3. TabView 标签视图
+ * 4. TabControllor 标签的控制器，控制按的是那个标签，打开的是那个标签视图
+ */
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
+
     return DefaultTabController(
-      length: 3,
+      length: 4,
       child: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
-          title: Text('coding type'),
-          elevation: 0.0,
-         /* leading: IconButton(
+          // 标题左边,设置抽屉，默认会加上
+          /* leading: IconButton(
             icon: Icon(Icons.menu),
             tooltip: 'Navigation',
-            onPressed: () => debugPrint('Navigation is onPress..'),
-          ),*/
-          // 标题右边的空间
+            onPressed: () => debugPrint('Navigation is onPress.'),
+          ), */
+          title: Text('coding type'), // 标题
+          // 标题右边
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.search),
               tooltip: 'Search',
-              onPressed: () => debugPrint('Search is onPress..'),
+              onPressed: () => debugPrint('Search is onPress.'),
             ),
           ],
-          // 标题底添加tabBar
+          elevation: 0.0, // 阴影 0.0  默认是 4.0
+          // 标题底部添加TabBar
           bottom: TabBar(
-              // 指示器
-              unselectedLabelColor: Colors.black38,
-              indicatorColor: Colors.black54,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 1.0,
-              // tab栏
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.local_florist)),
-                Tab(icon: Icon(Icons.change_history)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ]),
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+              Tab(icon: Icon(Icons.view_quilt)),
+            ],
+          ),
         ),
+        // 标签的内容显示在TabBarView
         body: TabBarView(
           children: <Widget>[
             ListViewDemo(),
             //Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            BasicDemo(),
+            //Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            LayoutDemo(),
+            //Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            SliverDemo(),
+            //Icon(Icons.view_quilt, size: 128.0, color: Colors.black12),
           ],
         ),
-        // 添加drawer部件
+        // 左边抽屉布局 右边的是endDrawer,里面一般用Drawer 部件
         drawer: DrawerDemo(),
-        // 添加底部部件
+        // 底部导航栏
         bottomNavigationBar: BottomNavigationBarDemo(),
       ),
     );

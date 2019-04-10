68c149779b988b61de54304f98663b7fb92574db
zhangkun@boxfish.cn <194264514@qq.com>
2019-04-10 14:26:50 +0800

    清除代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/demo/listview_demo.dart';
-import 'package:flutter_learn/demo/drawer_demo.dart';
-import 'package:flutter_learn/demo/bottom_navigation_bar_demo.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.yellow,
-        splashColor: Colors.white70, //水波纹的效果
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        backgroundColor: Colors.grey[100],
+      title: 'Weclome to Flutter',
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('coding type'),
-          elevation: 0.0,
-         /* leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation',
-            onPressed: () => debugPrint('Navigation is onPress..'),
-          ),*/
-          // 标题右边的空间
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: () => debugPrint('Search is onPress..'),
+          title: Text('Weclome to Flutter'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello JSPang  ,非常喜欢前端，并且愿意为此奋斗一生。我希望可以出1000集免费教程。',
+            textAlign: TextAlign.left,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 125, 125),
+              decoration: TextDecoration.underline, // 下划线
+              decorationStyle: TextDecorationStyle.dotted, // 下划线样式
+
             ),
-          ],
-          // 标题底添加tabBar
-          bottom: TabBar(
-              // 指示器
-              unselectedLabelColor: Colors.black38,
-              indicatorColor: Colors.black54,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 1.0,
-              // tab栏
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.local_florist)),
-                Tab(icon: Icon(Icons.change_history)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ]),
+          ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            ListViewDemo(),
-            //Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
-          ],
-        ),
-        // 添加drawer部件
-        drawer: DrawerDemo(),
-        // 添加底部部件
-        bottomNavigationBar: BottomNavigationBarDemo(),
       ),
     );
   }

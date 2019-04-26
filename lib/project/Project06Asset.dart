67b8f6c44105e05a01b916ea461401d2af8827ed
zhangkun@boxfish.cn <194264514@qq.com>
2019-04-26 23:34:57 +0800

    搜索框

---
+++ lib/project/Project06Asset.dart
@@ -0,0 +1,15 @@
+// 数据
+const searchList = [
+  "jiejie-大长腿",
+  "jiejie-大眼睛",
+  "jiejie-水蛇腰",
+  "xiaogege-小鲜肉",
+  "xiaogege-瓜子脸",
+  "xiaogege-大长腿",
+];
+
+const recentSuggest= [
+  "推荐-1",
+  "推荐-2",
+  "推荐-3",
+];
